71c136c4fa1eb19311fa1adba34abcb6284385b0
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-01-15 00:45:46 -0500

    DEFAULT

--- firewallup.sh
+++ firewallup.sh
@@ -1,8 +1,67 @@
 #! /usr/bin/env bash
+#NoGuiLinux
+function animus(){
+python2 << EOF
+import os,time
+import os,time
+
+max=10
+period=0.1
+defaultString=' '*max
+
+os.system('clear')
+
+row=list()
+for i in range(0,max):
+    row.append([i for i in defaultString])
+
+for i in range(0,max):
+ if i < max-1:
+  beam='-'
+ else:
+  beam='x'
+ row[i][i]=beam
+ for n in row[i]:
+  print("|"+''.join(row[i])+"|")
+
+ time.sleep(period)
+ os.system('clear')
+
+for i in reversed(range(0,max)):
+ if i > 0:
+  beam='-'
+ else:
+  beam='x'
+ row[i][i]=beam
+ for n in row[i]:
+  print("|"+''.join(row[i])+"|")
+
+ time.sleep(period)
+ os.system('clear')
+
+EOF
+}
+function customHeader(){
+	if test "$1" != "" ; then
+		banner="$1 -- ooh, you aren't the captain! Shields will not be raised! Enjoy Klingon Fire!"
+	else
+		banner="The Captain has ordered the shields to be raised! Raising the Shields!"
+	fi
+	animus
+	echo "$banner"
+}
 
 function rootUser(){
+	#if the user is not root, well, let them know, and exit 1
 	if test "`whoami`" != "root" ; then
-		echo "user is not root/sudo"
+		customHeader "User is not root/sudo"
+		exit 1
+	else
+		customHeader
+		initialize
+		portOpen
+		siteReject
+		exit 0
 	fi
 }
 
@@ -37,21 +96,46 @@ function initialize(){
 	iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
 	#reject all other incoming traffic
 	iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
-
-	#allow connections to port 80
-	#iptables -A TCP -p tcp --dport 80 -j ACCEPT
-	#allow connections to port 139,445,137,138,22,80
 }
 function portOpen(){
+	#trusted interface(s)
+	trustIface=("enp3s0")
+	#new connections '-m conntrack --ctstate NEW' will be directed from the INPUT chain to here first
 	portsTCP=("139" "445" "22" "80")
 	portsUDP=("137" "138")
-
+	#trusted network ports
+	SANportsTCP=("3306")
+	SANportsUDP=()
 	for i in ${portsTCP[@]} ; do
 		iptables -A TCP -p tcp --dport $i -j ACCEPT
 	done
 	
 	for i in ${portsUDP[@]}; do
 		iptables -A UDP -p udp --dport $i -j ACCEPT
+	done
+	#only allow MySQL on SAN
+	#allow ports to be open only on trusted network
+	#in this case my 'SAN'
+	for i in ${trustIface[@]}  ; do
+		for port in ${SANportsTCP[@]} ; do
+			#always check for blank values, and skip/warn
+			if test "$port" != "" ; then
+				iptables -A TCP -i "$i" -p tcp --dport "$port" -j ACCEPT
+			else
+				echo "$port : blank port value"
+			fi
+		done
+	done
+
+	for i in ${trustIface[@]}  ; do
+		for port in ${SANportUDP[@]} ; do
+			#always check for blank values, and skip/warn 
+			if test "$port" != "" ; then
+				iptables -A UDP -i "$i" -p udp --dport "$port" -j ACCEPT
+			else
+				echo "$port : blank port value"
+			fi
+		done
 	done
 }
 function siteReject(){
@@ -65,6 +149,3 @@ function siteReject(){
 	done
 }
 rootUser
-initialize
-portOpen
-siteReject
