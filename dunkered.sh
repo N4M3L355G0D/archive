2cdad68954020f7adeb86e0815ab447d7129c724
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-01-19 20:14:20 -0500

    DEFAULT

--- dunkered.sh
+++ dunkered.sh
@@ -3,6 +3,48 @@
 #gpg user
 User="Karl Josef Wisdom III"
 endCode="#stop"
+function banner(){
+python2 << EOF
+string="$1"
+stringTop=len(string)*'-'
+stringBot=len(string)*'='
+print '{}\n{}\n{}'.format(stringTop,string,stringBot)
+EOF
+}
+
+function checkDeps(){
+	commands=("gzip" "gunzip" "base64" "gpg" "sed")
+	allFail="no"
+	missing=()
+	counter=0
+	for cmd in ${commands[@]} ; do
+		export IFS=":"
+		exist=''
+		for pth in $PATH ; do
+			Exist="`ls $pth/$cmd |& grep -v "ls: cannot access"`"
+			if test "$Exist" != "" ; then
+				exist="$Exist"
+			fi
+		done
+		export IFS=" "
+		if test "$exist" == "" ; then
+			missing[$counter]="$cmd"
+			counter=`expr $counter + 1`
+		fi
+	done
+	export IFS=" "
+	if test "${#missing[@]}" -gt 0 ; then
+		banner missing
+		for miss in ${missing[@]} ; do
+			echo -e "-> $miss"
+		done
+		return 1
+	else
+		return 0
+	fi
+
+}
+
 function data(){
 	acc=''
 	message=''
@@ -43,8 +85,18 @@ function gpgDataD(){
 function gunzipData(){
 	gpgDataD | gunzip -d - -f
 }
-if test "$1" == "-e" ; then
-	dataToXclip
-elif test "$1" == "-d" ; then
-	gunzipData
-fi
+function main(){
+	if checkDeps ; then
+		if test "$1" == "-e" ; then
+			dataToXclip
+		elif test "$1" == "-d" ; then
+			gunzipData
+		else
+			echo -e "-d to decrypt\n-e to encrypt"
+		fi
+	else
+		banner "Some dependencies do not appear to be in your \$PATH"
+		exit 1
+	fi
+}
+main "$@"
