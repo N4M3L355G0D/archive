63fc96b99f63754ce5c2d8cba83aa7593d88f752
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-01-22 09:16:46 -0500

    DEFAULT

---
+++ bcard/bcard2.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+import base64,sqlite3,gzip,os
+data="""H4sIAFTuZVoAA+3YT0/CMBgG8Ps+zTbBxIOHttA5GJgOOtludE0osrLF4P7w6WXiTLyY
+mBi9PE/SN23f5Nf0+kqPL7abuAiDrM6tZzQjZ0IJIWJKBCM70ue9huRrpp9liHNZlPwsjLBR2hG3
+39PJbUP6NWC/FmDAgAEDBgwYMGDAgAEDBgwYMGDAgAH7tstDSts84SKg9CY2qW2L1OddFixNbgUT
+3oyLJOYhj6k4tNwJr41SWX7KVtTNNsaNgqxWVu50kIw0awv1xCvFhp6udGC8dH89z8XHVHTsEqcv
+f/NNYMCAAQMGDBgwYMCAAQMGDBgwYMCAAfs3jBqHCFnP5DIRPu8Uo/Nwaqrcjot8T2creRgtJuL0
+OJFutBbNcq1fIlu+boOiUzbpoqOucj+ulaTHfvzqKDZ+Vr5X6oe4yc9lHfnDHLa/d+ssSM5b/+4c
+bUwTXR5MfVNpK++dN3CKEC88KwAA"""
+dataDe=base64.b64decode(gzip.decompress(base64.b64decode(data.encode())))
+file=open("bc.db","wb")
+file.write(dataDe)
+file.close()
+#this file is intended for a 2X3 business card, thus no checking
+db=sqlite3.connect("bc.db")
+cursor=db.cursor()
+sql="select * from card;"
+cursor.execute(sql)
+for row in cursor.fetchall():
+    for col in row:
+        print(col)
+os.remove("bc.db")
