1774e88ca48d53663c4f39e7121defc2d4774d76
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-03-10 01:56:28 -0500

    DEFAULT

--- weatherForcast/weatherunderground.py
+++ weatherForcast/weatherunderground.py
@@ -30,58 +30,64 @@ class hourlyForecast:
         url=self.webpage(self.address)
         soup=bs(url,'html.parser')
         targetData=soup.find('table',{'id':'hourly-forecast-table'}).find_all('tr')
-        #set rowCast data
-        for i in targetData:
-            rowdata=i.find_all('td')
-            if len(rowdata) > 0:
-                time=rowdata[0].find_all('span')[0].text
-                conditions=rowdata[1].find_all('span')[1].text
-                temp=rowdata[2].find_all('span')[0].text
-                feelsLike=rowdata[3].find_all('span')[0].text
-                precip=rowdata[4].find_all('span')[0].text
-                amount=rowdata[5].find_all('span')[0].text
-                cloudCover=rowdata[6].find_all('span')[0].text
-                dewpoint=rowdata[7].find_all('span')[0].text
-                humidity=rowdata[8].find_all('span')[0].text
-                wind=rowdata[9].find_all('span')[0].text
-                pressure=rowdata[10].find_all('span')[0].text.replace('\n','')
-                self.rowCast={'time':time,'conditions':conditions,'temp':temp,'feels_like':feelsLike,'precip':precip,'amount':amount,'cloud_cover':cloudCover,'dew_point':dewpoint,'humidity':humidity,'wind':wind,'pressure':pressure}
-                self.table.append(self.rowCast)
+        if targetData != None:
+            #set rowCast data
+            for i in targetData:
+                rowdata=i.find_all('td')
+                if len(rowdata) > 0:
+                    time=rowdata[0].find_all('span')[0].text
+                    conditions=rowdata[1].find_all('span')[1].text
+                    temp=rowdata[2].find_all('span')[0].text
+                    feelsLike=rowdata[3].find_all('span')[0].text
+                    precip=rowdata[4].find_all('span')[0].text
+                    amount=rowdata[5].find_all('span')[0].text
+                    cloudCover=rowdata[6].find_all('span')[0].text
+                    dewpoint=rowdata[7].find_all('span')[0].text
+                    humidity=rowdata[8].find_all('span')[0].text
+                    wind=rowdata[9].find_all('span')[0].text
+                    pressure=rowdata[10].find_all('span')[0].text.replace('\n','')
+                    self.rowCast={'time':time,'conditions':conditions,'temp':temp,'feels_like':feelsLike,'precip':precip,'amount':amount,'cloud_cover':cloudCover,'dew_point':dewpoint,'humidity':humidity,'wind':wind,'pressure':pressure}
+                    self.table.append(self.rowCast)
+        else:
+            self.Table=None
 
 
 class display:
     table=[]
     def printTable(self):
-        header=[color.formatFront+i+color.formatReset for i in self.table[0].keys()]
-        orgHdLen=len(header)
-        longest=0
-        tableList=[]
-        #get  the longest table cell
-        for i in self.table:
-            for x in i.keys():
-                if len(i[x]) > longest:
-                    longest=len(i[x])
-        #adjust all table cells to longest table cell
-        for i in self.table:
-            row=[]
-            for x in i.keys():
-                if len(i[x]) <= longest:
-                    i[x]+=' '*(longest-len(i[x]))
-                    i[x]=color.colorFrontCell+i[x]+color.formatReset
-                row.append(i[x])
-            tableList.append('|'.join(row))
+        if self.table != None:
+            header=[color.formatFront+i+color.formatReset for i in self.table[0].keys()]
+            orgHdLen=len(header)
+            longest=0
+            tableList=[]
+            #get  the longest table cell
+            for i in self.table:
+                for x in i.keys():
+                    if len(i[x]) > longest:
+                        longest=len(i[x])
+            #adjust all table cells to longest table cell
+            for i in self.table:
+                row=[]
+                for x in i.keys():
+                    if len(i[x]) <= longest:
+                        i[x]+=' '*(longest-len(i[x]))
+                        i[x]=color.colorFrontCell+i[x]+color.formatReset
+                    row.append(i[x])
+                tableList.append('|'.join(row))
 
-        for num,i in enumerate(header):
-            if len(i)-len(color.formatFront+color.formatReset) <= longest:
-                header[num]+=' '*(longest-((len(i)-len(color.formatFront+color.formatReset))))
+            for num,i in enumerate(header):
+                if len(i)-len(color.formatFront+color.formatReset) <= longest:
+                    header[num]+=' '*(longest-((len(i)-len(color.formatFront+color.formatReset))))
         
-        header='|'.join(header)
-        header+='\n'+'{}-'.format(color.formatFrontCyan)*((len(header)-(len(color.formatFront+color.formatReset)*orgHdLen))-1)+color.formatReset
-        header=header.replace('|','{}|{}'.format(color.formatFrontCyan,color.formatReset))
-        header='>'+header
-        print(header)
-        for i in tableList:
-            print('>{}<'.format(i.replace('|','{}|{}'.format(color.formatFrontCyan,color.formatReset))))
+            header='|'.join(header)
+            header+='\n'+'{}-'.format(color.formatFrontCyan)*((len(header)-(len(color.formatFront+color.formatReset)*orgHdLen))-1)+color.formatReset
+            header=header.replace('|','{}|{}'.format(color.formatFrontCyan,color.formatReset))
+            header='>'+header
+            print(header)
+            for i in tableList:
+                print('>{}<'.format(i.replace('|','{}|{}'.format(color.formatFrontCyan,color.formatReset))))
+        else:
+            print("no data available at this time")
 
 class tenDayForecast:
     table=[]
@@ -96,27 +102,31 @@ class tenDayForecast:
         soup=bs(page,'html.parser')
         table=[]
         rowCast={}
-        specdata=soup.find('tbody').find_all('tr')
-        for i in specdata:
-            #day column
-            day=i.find('td',{'headers':'day'})
-            dayName=day.find('span',{'class':'date-time'}).text
-            dayDetail=day.find('span',{'class':'day-detail clearfix'}).text
-            day='{} {}'.format(dayName,dayDetail)
-            #description
-            desc=i.find('td',{'headers':'description'}).text
-            #temp
-            temp=i.find('td',{'headers':'hi-lo'})
-            temp='/'.join([i.text for i in temp.find_all('span') if i.text != ''])
-            #precip
-            precip=i.find('td',{'headers':'precip'}).find('span',{'class':''}).text
-            #wind
-            wind=i.find('td',{'headers','wind'}).find('span',{'class':''}).text
-            #humidity
-            humidity=i.find('td',{'headers','humidity'}).find('span',{'class':''}).text
-            #append to table
-            self.rowCast={'day':day,'desc':desc,'temp':temp,'precip':precip,'wind':wind,'humidity':humidity}
-            self.table.append(self.rowCast)
+        specdata=soup.find('tbody')
+        if specdata != None:
+            specdata=specdata.find_all('tr')
+            for i in specdata:
+                #day column
+                day=i.find('td',{'headers':'day'})
+                dayName=day.find('span',{'class':'date-time'}).text
+                dayDetail=day.find('span',{'class':'day-detail clearfix'}).text
+                day='{} {}'.format(dayName,dayDetail)
+                #description
+                desc=i.find('td',{'headers':'description'}).text
+                #temp
+                temp=i.find('td',{'headers':'hi-lo'})
+                temp='/'.join([i.text for i in temp.find_all('span') if i.text != ''])
+                #precip
+                precip=i.find('td',{'headers':'precip'}).find('span',{'class':''}).text
+                #wind
+                wind=i.find('td',{'headers','wind'}).find('span',{'class':''}).text
+                #humidity
+                humidity=i.find('td',{'headers','humidity'}).find('span',{'class':''}).text
+                #append to table
+                self.rowCast={'day':day,'desc':desc,'temp':temp,'precip':precip,'wind':wind,'humidity':humidity}
+                self.table.append(self.rowCast)
+        else:
+            self.table=None
 
 hourly=hourlyForecast()
 hourly.setTable()
