bde2ba2f0d824a57cb909d6145a6f3ed2696d495
noguilinux <k.j.hirner.wisdom@gmail.com>
2018-04-13 05:12:47 +0000

    updates

--- winbackup/zipper.py
+++ winbackup/zipper.py
@@ -5,11 +5,15 @@ import os,sys,argparse,zipfile
 path='/srv/samba/php'
 zipname='new.zip'
 
-def createZip(zipName,path):
+def createZip(zipName,path,FULLPATH=None):
     arcpath=os.path.split(path)[0]
     z=zipfile.ZipFile(zipName,'w',compression=zipfile.ZIP_LZMA)
     for root,dir,fnames in os.walk(path,topdown=True):
         for fname in fnames:
-            z.write(os.path.join(root,fname),os.path.join(root.replace(arcpath,''),fname))
+            if FULLPATH == None:
+                z.write(os.path.join(root,fname),os.path.join(root.replace(arcpath,''),fname))
+            else:
+                z.write(os.path.join(root,fname),os.path.join(root,fname))
 
-createZip(zipname,path)
+
+createZip(zipname,path,FULLPATH=True)
