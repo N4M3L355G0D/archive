649fb48e1e67dbe94c46b565b2a302fc5d1db0c0
noguilinux <k.j.hirner.wisdom@gmail.com>
2018-04-14 02:25:58 +0000

    updates

--- classing-python3/firstQt2.py
+++ classing-python3/firstQt2.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QToolTip,QPushButton,QApplication,QMainWindow,qApp,QAction,QMenu
+from PyQt5.QtWidgets import QApplication,QWidget,QToolTip,QPushButton,QApplication,QMainWindow,qApp,QAction,QMenu,QGridLayout
+
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
@@ -55,7 +56,7 @@ class orvil(QMainWindow):
         def initUi(self):
             QToolTip.setFont(QFont('SansSerif',12))
             self.setToolTip('This is is a <b>QWidget</b> widget')
-            self.setGeometry(300,300,300,220)
+            self.setGeometry(300,300,400,400)
             self.setWindowTitle('Icon')
             self.setWindowIcon(QIcon('tays handscrub.png'))
             self.show()
@@ -63,9 +64,6 @@ class orvil(QMainWindow):
     def initUI(self):
         #at this level, the var master found in other classes is self.Example
         self.Example=self.example()
-
-        self.Example.newBtn=self.nbt()
-        self.Example.newBtn.master=self.Example
         
         self.Example.cm=self.cm()
         self.Example.cm.master=self.Example
@@ -79,33 +77,49 @@ class orvil(QMainWindow):
 
         self.Example.buttons=self.buttons()
         self.Example.buttons.master=self.Example
+        '''
         self.Example.buttons.quitBtn()
         self.Example.buttons.okayBtn()
+        '''
+        self.Example.layout=self.layout()
+        self.Example.layout.master=self.Example
+        self.Example.layout.setLayout()
 
         self.Example.toolbar=self.toolbar()
         self.Example.toolbar.master=self.Example
         self.Example.toolbar.mkToolBar()
 
         self.Example.initUi()
-
+    class layout(example):
+        master=None
+        def setLayout(self):
+            self.wid=QWidget(self.master)
+            self.master.setCentralWidget(self.wid)
+            self.grid=QGridLayout()
+            self.grid.addWidget(self.master.buttons.okayBtn(),1,0)
+            self.grid.addWidget(self.master.buttons.quitBtn(),1,1)
+            self.wid.setLayout(self.grid)
+            
     class buttons(example):
         master=None
         #gui buttons
         def quitBtn(self):
-            button=QPushButton('Quit',self.master)
+            button=QPushButton('Quit')
             button.setToolTip('<b>Quit</b>!')
-            button.resize(100,50)
-            button.move(150,150)
+            button.resize(50,50)
+            #button.move(150,150)
             button.setStatusTip('Quit Button')
             button.clicked.connect(lambda: self.master.act.quit(self.master.statusBar(),QApplication.instance()))
+            return button
        
         def okayBtn(self):
-            button=QPushButton('Okay',self.master)
+            button=QPushButton('Okay')
             button.setToolTip('<b>Okay</b>!')
-            button.resize(100,50)
-            button.move(50,150)
+            button.resize(50,50)
+            #button.move(50,150)
             button.setStatusTip('Okay Button')
             button.clicked.connect(lambda: self.master.act.okay(self.master.statusBar()))
+            return button
 
     class fileMenu(example):
         #toolbar file menu
@@ -144,7 +158,6 @@ class orvil(QMainWindow):
         def mkFileAction_New(self):
             action=QAction('New',self.master)
             action.setStatusTip('New')
-            action.setShortcut("Ctrl+N")
             action.triggered.connect(lambda: self.master.newBtn.newBtnStatus())
             return action
 
@@ -170,40 +183,17 @@ class orvil(QMainWindow):
 
         def contextMenuActNew(self,cmenu):
             action=cmenu.addAction("New")
-            action.setShortcut("Ctrl+N")
             #need to pass self into newBtn to make it aware of the window to connect to 
             action.triggered.connect(lambda: self.master.newBtn.newBtnStatus())
             self.setStatusTip("New")
-            return action
+            return action 
 
-    class nbt(example):
-            newBtnCreated=False
-            master=None 
-            def newBtnHide(self,button):
-                self.newBtnCreated=False
-                button.hide()
-        
-            def newBtn(self):
-                button=QPushButton('New',self.master)
-                button.setToolTip('<b>It\'s a New button</b>!')
-                button.resize(100,50)
-                button.move(50,100)
-                button.setStatusTip('New Button')
-                button.clicked.connect(lambda: self.newBtnHide(button))
-
-                return button
-    
-            def newBtnStatus(self):
-                if self.newBtnCreated == False:
-                    button=self.newBtn()
-                    button.show()    
-                    self.newBtnCreated=True
-                print(self.newBtnCreated)
     class toolbar(example):
         master=None
         def mkToolBar(self):
             self.toolbar=self.master.addToolBar('exit')
             action=self.master.fm.exitAction()
+            action.setShortcut('')
             self.toolbar.addAction(action)
 
     def __init__(self):
