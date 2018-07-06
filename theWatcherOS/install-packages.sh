ccbc01885af451ccf644ecea980a7edf364d4587
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-07-06 10:35:07 -0700

    DEFAULT

--- theWatcherOS/install-packages.sh
+++ theWatcherOS/install-packages.sh
@@ -2,22 +2,22 @@
 #NoGuiLinux
 
 if test `whoami` == "root" ; then
-	python3 whichIsIt.py > installed.txt
+	#python3 whichIsIt.py > installed.txt
 	grep "OFFICIAL" installed.txt | cut -f1 -d: > official.txt
 
 	#make container
 	mkdir container
-	pacstrap -i container base base-devel $(cat official.txt)
+	pacstrap --config ./pacman.conf -i /mnt base base-devel $(cat official.txt)
 	
 	grep "AUR" installed.txt | cut -f1 -d: > aur.txt
 
-	cp aur.txt yaourt-install.sh install-aur.sh container/root/
+	cp aur.txt yaourt-install.sh install-aur.sh /mnt/root/
 
 
-	genfstab container >>> container/etc/fstab
+	genfstab /mnt > /mnt/etc/fstab
 	#boot container
 	#systemd-nspawn -b -D container
-	arch-chroot containter
+	arch-chroot /mnt
 	#log in to root
 	#run rootlogin.sh
 	##run containerlogin.sh
