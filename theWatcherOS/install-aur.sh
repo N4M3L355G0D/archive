47ca482655f40baa4648f3c0b6f5769975904c06
kohmet <k.j.hirner.wisdom@gmail.com>
2019-01-05 15:54:26 -0800

    DEFAULT

--- theWatcherOS/install-aur.sh
+++ theWatcherOS/install-aur.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #NoGuiLinux
-
+cmd='yaourt'
 function manualBuild(){
 	#handle getting dependencies
         #need to get a complete list dependencies
@@ -18,13 +18,14 @@ function manualBuild(){
                 rm -r $i 
         fi
 }
-
+$cmd -S yay
+cmd='yay'
 for i in `cat aur.txt` ; do 
         installed=`pacman -Qq $i`
         if test "$i" != "$installed" ; then
 		inLog="`fgrep -w "$i" fail.txt`"
 		if test "$inLog" == "" ; then
-	                yaourt -S $i
+	                $cmd -S $i |& tee action_log.txt
         	        if test $? != 0 ; then 
  			read -rp "build manually?[y/n]:" ans
         			if test "$ans" == "y" ; then
